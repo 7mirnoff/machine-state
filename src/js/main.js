34946819b92248ca2313fbc424e678e16063bb81
Dmitry Shamrilo <shamrilods@gmail.com>
2019-10-04 10:33:51 +0300

    Рефакторит код

--- src/js/main.js
+++ src/js/main.js
@@ -7,18 +7,10 @@ import {
 let stateMachine = new StateMachine()
 stateMachine.setState(stateList.begin)
 
-const buttonStage1 = document.querySelector(`.state-machine__button--1`)
-const buttonStage2 = document.querySelector(`.state-machine__button--2`)
-const buttonStage3 = document.querySelector(`.state-machine__button--3`)
+const buttonsStage = document.querySelectorAll(`.state-machine__button`)
 
-buttonStage1.addEventListener(`click`, () => {
-  stateMachine.setState(stateList.stage1)
-})
-
-buttonStage2.addEventListener(`click`, () => {
-  stateMachine.setState(stateList.stage2)
-})
-
-buttonStage3.addEventListener(`click`, () => {
-  stateMachine.setState(stateList.stage3)
+buttonsStage.forEach((button, i) => {
+  button.addEventListener(`click`, () => {
+    stateMachine.setState(stateList[`stage${i + 1}`])
+  })
 })
