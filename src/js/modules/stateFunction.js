fbb86eb4640102ea750f1d78164978eced6cf5f5
Dmitry <smirnov.dmitry.a@mail.ru>
2019-10-07 12:17:07 +0300

    animation state

--- src/js/modules/stateFunction.js
+++ src/js/modules/stateFunction.js
@@ -1,17 +1,40 @@
+import anime from 'animejs'
+
 const stage0 = document.querySelector(`.state-machine__stage--0`)
 const stage1 = document.querySelector(`.state-machine__stage--1`)
 const stage2 = document.querySelector(`.state-machine__stage--2`)
 const stage3 = document.querySelector(`.state-machine__stage--3`)
 
 const hideStage0 = () => {
-  stage0.style.display = `none`
+  return anime({
+    targets: stage0,
+    opacity: 0,
+    translateY: -100,
+    complete() {
+      stage0.style.display = `none`
+    }
+  })
 }
 
 const showStage1 = () => {
-  stage1.style.display = `block`
+  return anime({
+    targets: stage1,
+    opacity: 1,
+    translateY: [100, 0],
+    begin() {
+      stage1.style.display = `block`
+    }
+  })
 }
 const hideStage1 = () => {
-  stage1.style.display = `none`
+  return anime({
+    targets: stage1,
+    opacity: 0,
+    translateY: -100,
+    complete() {
+      stage1.style.display = `none`
+    }
+  })
 }
 const showStage2 = () => {
   stage2.style.display = `block`
